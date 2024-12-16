be65e68cab5cefdcc7702e2a3b1c75a91c0ea5a7
Yuska <yuska@pts.org.tw>
2024-12-16 14:13:17 +0800

    fix: fix query string not sync with ui state

--- app/form-preview/page.tsx
+++ app/form-preview/page.tsx
@@ -2,34 +2,50 @@
 
 import { formItems } from '@/components/FormItem'
 import { FormItemConfig } from '@/components/FormItem/types'
+import SaveButton from '@/components/SaveButton'
 import { Form } from '@/components/ui/Form'
 import { ScrollArea } from '@/components/ui/ScrollArea'
+import { Separator } from '@/components/ui/Separator'
+import useBroadcast from '@/hooks/useBroadcast'
+import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
-import { useSearchParams } from 'next/navigation'
-
-import useBroadcast from '@/hooks/useBroadcast'
-
 export default function Preview() {
+  const [status, setStatus] = useState<'idle' | 'loading' | 'success'>('idle')
   const params = useSearchParams()
-  const initConfigs = JSON.parse(
-    params.get('configs') || '[]'
-  ) as FormItemConfig[]
-  const [configs, setConfigs] = useState<FormItemConfig[]>(initConfigs)
-
-  const form = useForm()
+  const [configs, setConfigs] = useState<FormItemConfig[]>(() => {
+    const configsQuery = params.get('configs') || '[]'
+    return JSON.parse(configsQuery) as FormItemConfig[]
+  })
 
   const { listen } = useBroadcast('preview-form')
   useEffect(() => {
     listen((configs) => {
       setConfigs(configs)
+      window.history.replaceState(
+        { ...window.history.state },
+        '',
+        '/form-preview?configs=' + encodeURIComponent(JSON.stringify(configs))
+      )
     })
   }, [listen])
 
+  const form = useForm()
+
+  async function submit() {
+    setStatus('loading')
+    await form.handleSubmit(() => {
+      setStatus('success')
+    })()
+    console.log(form.getValues())
+    console.log(form.formState)
+    return open
+  }
+
   return (
-    <div className="container m-auto">
-      <ScrollArea className="flex-grow">
+    <div className="container m-auto flex h-screen flex-col">
+      <ScrollArea>
         <div className="p-4">
           <Form {...form}>
             <form className="mt-4 grid grid-cols-12 gap-4">
@@ -41,6 +57,10 @@ export default function Preview() {
           </Form>
         </div>
       </ScrollArea>
+      <Separator />
+      <div className="flex justify-center p-4">
+        <SaveButton status={status} onClick={submit} />
+      </div>
     </div>
   )
 }
